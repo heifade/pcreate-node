f08673cc9a58d9bbe7431a2af703505f6f3c1d42
heifade <heifade@126.com>
2017-12-23 18:56:46 +0800

    no message

--- src/bin/pcreate-node.cmd
+++ src/bin/pcreate-node.cmd
@@ -1,7 +1,7 @@
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\pcreate\bin\pcreate" %*
+  "%~dp0\node.exe"  "%~dp0\..\pcreate\bin\pcreate-node" %*
 ) ELSE (
   @SETLOCAL
   @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\pcreate\bin\pcreate" %*
+  node  "%~dp0\..\pcreate-node\bin\pcreate-node" %*
 )
